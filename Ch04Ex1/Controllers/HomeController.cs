8c1173cbec6470460a010a57d4b6ec25ef5c993c
Parker <ptown@DESKTOP-4C33OP4>
2023-10-01 22:56:02 -0500

    after step 19

--- Ch04Ex1/Controllers/HomeController.cs
+++ Ch04Ex1/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ch04Ex1.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ch04Ex1.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context {  get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
     }
 }
